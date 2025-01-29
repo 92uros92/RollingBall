5c51edb2d08fb5f441111bd3ca2f883a3f9a07fc
92uros92 <122689123+92uros92@users.noreply.github.com>
2025-01-29 20:21:18 +0100

    - Added TeleportBox() class

---
+++ Source/RollingBall/Private/TeleportBox.cpp
@@ -0,0 +1,58 @@
+// Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
+
+
+#include "TeleportBox.h"
+#include "BallPawn.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/Controller.h"
+#include "Components/StaticMeshComponent.h"
+
+
+
+
+ATeleportBox::ATeleportBox()
+{
+	//TeleporterMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TeleporterMesh"));
+
+	OnActorBeginOverlap.AddDynamic(this, &ATeleportBox::OnTeleporterEnter);
+	OnActorEndOverlap.AddDynamic(this, &ATeleportBox::OnTeleporterExit);
+
+	bIsTeleporting = false;
+}
+
+void ATeleportBox::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ATeleportBox::OnTeleporterEnter(AActor* OverlappActor, AActor* OtherActor)
+{
+	if (OtherActor && OtherActor != this)
+	{
+		if (OtherTeleporter)
+		{
+			ABallPawn* MyPawn = Cast<ABallPawn>(OtherActor);
+
+			if (MyPawn && !OtherTeleporter->bIsTeleporting)
+			{
+				bIsTeleporting = true;
+
+				MyPawn->SetActorRotation(OtherTeleporter->GetActorRotation());
+				MyPawn->GetController()->SetControlRotation(MyPawn->GetActorRotation());
+
+				MyPawn->SetActorLocation(OtherTeleporter->GetActorLocation());
+			}
+		}
+	}
+}
+
+void ATeleportBox::OnTeleporterExit(AActor* OverlappActor, AActor* OtherActor)
+{
+	if (OtherActor && OtherActor != this)
+	{
+		if (OtherTeleporter && !bIsTeleporting)
+		{
+			OtherTeleporter->bIsTeleporting = false;
+		}
+	}
+}
---
+++ Source/RollingBall/Public/TeleportBox.h
@@ -0,0 +1,39 @@
+// Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerBox.h"
+#include "TeleportBox.generated.h"
+
+
+class AActor;
+class UStaticMeshComponent;
+
+
+UCLASS()
+class ROLLINGBALL_API ATeleportBox : public ATriggerBox
+{
+	GENERATED_BODY()
+	
+public:
+
+	UPROPERTY(EditAnywhere, Category = "Teleporter")
+	ATeleportBox* OtherTeleporter;
+
+	UPROPERTY()
+	bool bIsTeleporting;
+
+
+	ATeleportBox();
+
+	UFUNCTION()
+	void OnTeleporterEnter(AActor* OverlappActor, AActor* OtherActor);
+
+	UFUNCTION()
+	void OnTeleporterExit(AActor* OverlappActor, AActor* OtherActor);
+
+protected:
+
+	virtual void BeginPlay() override;
+};
