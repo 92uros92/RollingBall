9403d67855a570dac5bdad3b3552d8c33e451467
92uros92 <122689123+92uros92@users.noreply.github.com>
2025-03-03 22:00:25 +0100

    - Create MainMenu_GameMode() class - In editor changed BP_MainMenu_BM

---
+++ Source/RollingBall/Public/UI/MainMenu_GameMode.h
@@ -0,0 +1,17 @@
+// Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MainMenu_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROLLINGBALL_API AMainMenu_GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
